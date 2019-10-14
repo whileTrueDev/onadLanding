9f3830468a4cbf92e9716564b7552b0b9ac5f85e
hwasurr <iamsupermazinga@gmail.com>
2019-10-14 18:21:00 +0900

    api 주소 변경 / nginX,dockerfile 주소 변경

--- client/src/config/host.js
+++ client/src/config/host.js
@@ -1,3 +1,3 @@
-const apiHOST = process.env.NODE_ENV === 'production' ? 'https://landing.onad.io' : 'http://localhost:3010';
+const apiHOST = process.env.NODE_ENV === 'production' ? 'https://l-api.onad.io' : 'http://localhost:3010';
 
 export default apiHOST;
