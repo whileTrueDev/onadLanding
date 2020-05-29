cfdb723dca5c3ced56fc5484c9002f7df6b02973
dn0208@gmail.com <dn0208@gmail.com>
2020-05-29 14:02:43 +0900

    [UPDATE #7.3] 광고페이지 service 디렉토리 삭제

--- server/middleware/landingActionLogging.js
+++
@@ -1,8 +0,0 @@
-function actionLogging(a, b, c) {
-  const date = new Date().toLocaleString();
-  const log = `[${date}] [${a}] [${b}] ${c}`;
-  console.log(log);
-  return log;
-}
-
-module.exports = actionLogging;
